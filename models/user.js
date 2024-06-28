64a0289d570569aed9a54ce4769bfc6df542881f
mackemo <mackenzielmoore14@gmail.com>
2024-06-27 21:54:35 -0400

    created hooks for before creating and updating a user to hash password, imported sequelize and made model name user, exported model

--- models/user.js
+++ models/user.js
@@ -41,10 +41,30 @@ User.init(
             isEmail: true,
             },
         },
-        
+
         password: {
             type: DataTypes.STRING,
             allowNull: false,
         }, 
     },
-)
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+        },
+            beforeUpdate: async (updatedUserData) => {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+                }
+        },
+
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "user",
+    },
+);
+
+module.exports = User;
