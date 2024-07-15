db160bdf87bbaab153510778a69a5b02b51dd382
mackemo <mackenzielmoore14@gmail.com>
2024-07-15 16:42:30 -0400

    route to retrieve a single post with its comments

--- controllers/api/post-route.js
+++ controllers/api/post-route.js
@@ -22,4 +22,38 @@ router.get('/', async (req, res) => {
 });
 
 
+// retrieve single post with corresponding comments --------------
+router.get('/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            attributes: ['post_title', 'post_text'],
+            // displays post title and text
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['comment_text'],
+                    // includes comments on posts
+                    include: [
+                        {
+                            model: User,
+                            attributes: ['first_name', 'last_name']
+                            // includes the user's names that commented
+                        }
+                    ]
+                }
+            ]
+        });
+
+        if (!postData) {
+            res.status(404).json({ message: 'Post not found' });
+            return;
+        }
+
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
 module.exports = router;
