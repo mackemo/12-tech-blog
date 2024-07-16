fbcbc6357a429790f9826180048d4c682b093dd4
mackemo <mackenzielmoore14@gmail.com>
2024-07-15 22:19:23 -0400

    get route to render single user posts on dashboard

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,4 +30,46 @@ router.get('/', async (req, res) => {
 });
 
 
+// ---------Dashboard -----------------
+router.get('/dashboard', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            attributes: ['post_title', 'post_text'],
+            include: [
+                {
+                    model: User,
+                    as: 'author',
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['comment_text'],
+                    // includes comments on posts
+                    include: [
+                        {
+                            model: User,
+                            attributes: ['username']
+                            // includes the user's names that commented
+                        }
+                    ]
+                }
+            ]
+        })
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        // render to dashboard
+        res.render('dashboard', {
+            posts,
+            loggedIn: req.session.loggedIn
+        });
+       
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+    
+
 module.exports = router;
