64c5e86b3799f0830d2b4afeacb198378f90d732
mackemo <mackenzielmoore14@gmail.com>
2024-07-24 16:32:43 -0400

    included comments in login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,20 +1,25 @@
+// logging in
 const loginFormHandler = async (event) => {
     event.preventDefault();
-  
+    
+    // grab username and password
     const username = document.querySelector('#username').value.trim();
     const password = document.querySelector('#password').value.trim();
 
+    // if neither inputted, alert
     if (!username || !password) {
         alert('Please fill in both username and password');
     }
-  
+    
+    // if both included, fetch the login route
     if (username && password) {
         const response = await fetch('/api/users/login', {
             method: 'POST',
             body: JSON.stringify({ username, password }),
             headers: { 'Content-Type': 'application/json' },
         });
-  
+        
+        // go to homepage
         if (response.ok) {
             document.location.replace('/');
         } else {
@@ -22,6 +27,7 @@ const loginFormHandler = async (event) => {
         }
     }
 };
-  
+
+// on clicking login, function runs
 document.querySelector('#login').addEventListener('submit', loginFormHandler);
 
