ad2427c1acf07a685086b90e1aadb4db384c9ac8
mackemo <mackenzielmoore14@gmail.com>
2024-07-24 17:10:21 -0400

    implemented front end js for editting a post

--- public/js/editpost.js
+++ public/js/editpost.js
@@ -1,19 +1,24 @@
-// creating a post
-const createPostFormHandler = async (event) => {
+// editting a post
+const editPostFormHandler = async (event) => {
     event.preventDefault();
     
     // grab title and text
-    const title = document.querySelector('#title').value;
-    const text = document.querySelector('#text').value;
+    const title = document.querySelector('#edit-title').value;
+    const text = document.querySelector('#edit-text').value;
 
     // if neither inputted, alert
     if (!title || !text) {
         alert('Please fill in title and text');
     }
     
+    // grab post id
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
     // if both included, fetch the post route
     if (title && text) {
-        const response = await fetch('/api/posts/', {
+        const response = await fetch(`/api/posts/${id}`, {
             method: 'POST',
             body: JSON.stringify({ title, text }),
             headers: { 'Content-Type': 'application/json' },
@@ -23,10 +28,10 @@ const createPostFormHandler = async (event) => {
         if (response.ok) {
             document.location.replace('/dashboard');
         } else {
-            alert('Failed to create post');
+            alert('Failed to edit post');
         }
     }
 };
 
-// on clicking create, function runs
-document.querySelector('#add-post').addEventListener('submit', createPostFormHandler);
+// on clicking save, function runs
+document.querySelector('#save-post').addEventListener('submit', editPostFormHandler);
