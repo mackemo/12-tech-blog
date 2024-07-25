d5f4dffa69bff6114bfa51628941330cc3ee7d80
mackemo <mackenzielmoore14@gmail.com>
2024-07-25 19:05:36 -0400

    deleteuser.js includes funcitonality for deleting a user, grabbing user by id, fetching delete route and returning to homepage

--- public/js/deleteuser.js
+++ public/js/deleteuser.js
@@ -1,26 +1,26 @@
-// deleting a post
-const deletePost = async (event) => {
+// deleting a user
+const deleteUser = async (event) => {
     event.preventDefault();
     
-    // grab post id
+    // grab user id
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
 
     // fetch delete route
-    const response = await fetch(`/api/posts/${id}`, {
+    const response = await fetch(`/api/users/${id}`, {
         method: 'DELETE',
         body: JSON.stringify({ id }),
         headers: { 'Content-Type': 'application/json' },
     });
         
-        // go to dashboard
+        // go to homepage
         if (response.ok) {
-            document.location.replace('/dashboard');
+            document.location.replace('/');
         } else {
-            alert('Failed to delete post');
+            alert('Failed to delete user');
         }
 };
 
 // on clicking delete, function runs
-document.querySelector('#delete-post').addEventListener('submit', deletePost);
+document.querySelector('#delete-usert').addEventListener('submit', deleteUser);
