b5eb33a8ab38d60db7b06c0cf86dcdcd5a581c3a
mackemo <mackenzielmoore14@gmail.com>
2024-08-15 15:46:17 -0400

    Changed connection to not include DB_URL

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
+const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
